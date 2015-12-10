343654bd77b7d227a6331732d61f6085d67aa861
jciancio <jciancio7@gmail.com>
2015-12-09 22:40:18 -0500

    first test in user registers pass

--- spec/features/user_registers_spec.rb
+++ spec/features/user_registers_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 feature 'Registering' do
-  it 'displays a sign up form'
+  before do
+    visit new_user_registration_path
+  end
+
+  it 'displays a sign up form' do
+    find_field('Email')
+    find_field('Password')
+    find_field('Password confirmation')
+    find_button('Sign up')
+  end
+
   it 'submitting the form creates a user'
 end
