b93677a5551b71eae843c76d981b83f8f6305c76
jciancio <jciancio7@gmail.com>
2015-12-09 23:02:29 -0500

    both tests passing in user logging in test

--- spec/features/user_logs_in_sepc.rb
+++ spec/features/user_logs_in_sepc.rb
@@ -3,8 +3,19 @@ require 'rails_helper'
 feature 'Logging In' do
   before do
     visit new_user_session_path
+    @user = User.create(email: 'example@example.com', password: 'password')
   end
 
-  it 'displays a login form'
-  it 'submitting the form creates a new session'
+  it 'displays a login form' do
+    find_field('Email')
+    find_field('Password')
+    find_button('Log in')
+  end
+
+  it 'submitting the form creates a new session' do
+    fill_in('Email', with: @user.email)
+    fill_in('Password', with: @user.password)
+    click_button('Log in')
+    expect(current_path).to eq(homepage_path)
+  end
 end
