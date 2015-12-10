5811c30674c2c224eabfb2e98c40ca77d828bca0
jciancio <jciancio7@gmail.com>
2015-12-09 22:05:31 -0500

    test now passing after hmbt associations written and migration created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210025642) do
+ActiveRecord::Schema.define(version: 20151210030452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,11 @@ ActiveRecord::Schema.define(version: 20151210025642) do
     t.integer  "message_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "comments", ["message_id"], name: "index_comments_on_message_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
@@ -54,5 +56,6 @@ ActiveRecord::Schema.define(version: 20151210025642) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "comments", "messages"
+  add_foreign_key "comments", "users"
   add_foreign_key "messages", "users"
 end
