ea4880609639fb5999ab4a4d642c1d3b71fa0948
jciancio <jciancio7@gmail.com>
2015-12-09 21:58:28 -0500

    generated migration to add foreign key to message model for user, added hmbt relationship, test now passes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151206232156) do
+ActiveRecord::Schema.define(version: 20151210025642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,10 @@ ActiveRecord::Schema.define(version: 20151206232156) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -51,4 +54,5 @@ ActiveRecord::Schema.define(version: 20151206232156) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "comments", "messages"
+  add_foreign_key "messages", "users"
 end
