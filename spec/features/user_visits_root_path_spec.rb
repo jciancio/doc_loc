fc4849f0488ac75c904e3862ac686cafce6a86e1
jciancio <jciancio7@gmail.com>
2015-12-09 22:15:20 -0500

    added pending tests for user visitng root feature test

--- spec/features/user_visits_root_path_spec.rb
+++ spec/features/user_visits_root_path_spec.rb
@@ -1,2 +1,8 @@
 require 'rails_helper'
 
+feature 'Visitng Root' do
+  it 'displays a link to login'
+  it 'and clicking Login goes to login path'
+  it 'displays a link to sign up'
+  it 'and clicking Sign Up goes to sign up path'
+end
