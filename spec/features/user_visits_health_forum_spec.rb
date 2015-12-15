cf7f24b24f9c62ea806b234c014f4d8319a8dbb4
jciancio <jciancio7@gmail.com>
2015-12-15 02:12:29 -0500

    last tests working, I have a user signature being displayed and am letting user know how long ago the comment was created

--- spec/features/user_visits_health_forum_spec.rb
+++ spec/features/user_visits_health_forum_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'Visiting Health Forum' do
+  include ActionView::Helpers
   before do
     @user = User.create(email: 'example@example.com', password: 'password')
     (1..100).each do |i|
@@ -27,6 +28,12 @@ feature 'Visiting Health Forum' do
   it 'displays a comment count on each message' do
     expect(page).to have_content(@message.comments.count)
   end
-  it 'displays a timestamp on each message'
-  it 'displays a user signature on each message'
+
+  it 'displays a timestamp on each message' do
+    expect(page).to have_content(time_ago_in_words(@message.created_at))
+  end
+
+  it 'displays a user signature on each message' do
+    expect(page).to have_content(@message.user.email)
+  end
 end
