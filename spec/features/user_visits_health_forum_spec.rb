7069df6cbf91ef6a55f8816a4c759231df36851b
jciancio <jciancio7@gmail.com>
2015-12-15 01:32:44 -0500

    have first two test working. decided to create 100 messages to check that they are all being shown. will also come in handy form time and signature stamps

--- spec/features/user_visits_health_forum_spec.rb
+++ spec/features/user_visits_health_forum_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 feature 'Visiting Health Forum' do
   before do
     @user = User.create(email: 'example@example.com', password: 'password')
-    @message = Message.create(tite: 'example', description: 'example', user_id: @user.id)
+    100.times do |i|
+      @message = Message.create(title: "example #{i}", description: "example #{i}", user_id: @user.id)
+    end
     visit new_user_session_path
     fill_in('Email', with: @user.email)
     fill_in('Password', with: @user.password)
@@ -11,8 +13,16 @@ feature 'Visiting Health Forum' do
     click_link('Health Forum')
   end
 
-  it 'displays a new message form'
-  it 'displays all messages'
+  it 'displays a new message form' do
+    find_field('topic')
+    find_field('Description')
+    find_button('Create Message')
+  end
+
+  it 'displays all messages' do
+    expect(page).to have_content(@message.title)
+  end
+
   it 'displays a comment count on each message'
   it 'displays a timestamp on each message'
   it 'displays a user signature on each message'
